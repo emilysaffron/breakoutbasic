50ef88665db3454decdad8ed056d06b449caae9b
emilysaffron <emiy.saffron@bbc.co.uk>
2021-02-16 10:38:42 +0000

    console log server

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-// viewed at http://localhost:8080
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
+console.log("viewed at http://localhost:8080");
 
 app.listen(8080);
